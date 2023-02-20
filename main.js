3a1c702370d937008fd6a62451d1019241fb10f5
Ricardo Cardim <ricardo.cardim@virtualplus.pt>
2023-02-20 21:30:13 +0000

    atulizacao main

--- main.js
+++ main.js
@@ -4,4 +4,13 @@ function tocaSomPom () {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+//enquanto
+while (contador < 9){
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+} 
